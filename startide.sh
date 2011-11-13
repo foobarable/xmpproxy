3d0536a01b261b9433f1d03ad9ddea3b7d3532d1
Ralph Krimmel <mail@postefix.de>
2011-11-13 12:10:34 +0100

    added gvim and pida as options for editing

--- startide.sh
+++ startide.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 TERM=rxvt-256color vim -p xmpproxy.pl lib/DJabberd/*/* lib/xmpproxy/*
+#TERM=rxvt-256color gvim -p xmpproxy.pl lib/DJabberd/*/* lib/xmpproxy/*
+#LD_PRELOAD=/usr/lib/libpython2.7.so pida xmpproxy.pl lib/DJabberd/*/* lib/xmpproxy/*
+
