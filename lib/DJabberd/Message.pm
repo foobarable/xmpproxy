90819f09e6a2e845edc31e9baa98efbf861f45eb
Ralph Krimmel <mail@postefix.de>
2011-11-27 18:39:59 +0100

    Merged DJabberd::Message and DJabberd::Client::Message

--- lib/DJabberd/Message.pm
+++ lib/DJabberd/Message.pm
@@ -16,6 +16,21 @@ sub on_recv_from_client {
     $self->deliver;
 }
 
+sub on_recv_from_server {
+	my ($self, $conn) = @_;
+	
+	my $to = $self->to_jid;
+	if (! $to) {
+		warn "Can't process a message to nobody.\n";
+	 	$conn->close;
+		return;
+	}
+	my $newto = DJabberd::JID->new($xmpproxy::userdb->{proxy2local}->{$to->as_bare_string});
+	$self->set_to($newto);
+	$DJabberd::Stats::counter{"c2s-Message"}++;
+	$self->deliver;
+}
+
 sub process {
     my ($self, $conn) = @_;
     die "Can't process a to-server message?";
